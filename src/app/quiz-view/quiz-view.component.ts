0732397be76f71c85f884eaf0dd9485c7a1d7e18
aniadewor <116655947+aniadewor@users.noreply.github.com>
2025-03-11 18:42:31 +0100

    add view by role

--- src/app/quiz-view/quiz-view.component.ts
+++ src/app/quiz-view/quiz-view.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import {Role} from '../models/role.enum'
 @Component({
   selector: 'app-quiz-view',
   templateUrl: './quiz-view.component.html',
   styleUrl: './quiz-view.component.css'
 })
-export class QuizViewComponent {
-
+export class QuizViewComponent implements OnInit{
+  user: User= new User()
+  ngOnInit() {
+    this.user.role = Role.TEACHER
+  }
 }
